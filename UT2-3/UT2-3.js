2fbd11ad9d002ee1c4560dea2edaec2aac9a50ed
DaniGG832 <daniel.gonzalez.garcia@iesdonana.org>
2021-10-23 01:34:30 +0200

    ejercicios34

--- UT2-3/UT2-3.js
+++ UT2-3/UT2-3.js
@@ -1,18 +1,68 @@
 'use strict'
-
-let valorSala = document.getElementById("valorSala").value ;
-let valorSospechoso = document.getElementById("valorSospechoso").value ;
-
-
-
-let asesino =document.getElementById("asesino");
-let sospechoso =document.getElementById("sospechoso");
-let arma =document.getElementById("arma");
-
+// llamada a la funcon a traves de un boton
 document.getElementById("boton").onclick = comprobar;
 
+// variables 
+let asesino =document.getElementById("asesino");
+let sala =document.getElementById("sala");
+let arma =document.getElementById("arma");
+let resuelto =document.getElementById("resuelto");
+
+
+// funcion 
 function comprobar() {
-  if (valorSala==valorSospechoso) {
-     document.getElementById("asesino").innerHTML= document.getElementById("valorSala").value.innerHTML; 
+  let valorSala= document.getElementById("valorSala").value ;
+  let valorSospechoso = document.getElementById("valorSospechoso").value;
+
+  // muestra por consola los registros y se acumula cada vez que damos al boton 
+  console.log("----");
+  console.log(valorSala+"-"+valorSospechoso);
+  
+
+  if ((valorSala =="salón de baile" && valorSospechoso =="Sr. Kalehoff") ||
+      (valorSala =="Galería" && valorSospechoso =="Sra. Van Cleve")||
+      (valorSala =="Sala de billar" && valorSospechoso =="Sra. Sparr")||
+      (valorSala =="Comedor" && valorSospechoso =="Sr. Parkes")) {
+
+     asesino.innerHTML = valorSospechoso; 
+     sala.innerHTML = valorSala; 
+     
+        // este switch asocia el arma con la sala seleccionada
+        switch (valorSala) {
+          case "salón de baile":
+            arma.innerHTML = "veneno";
+            break;
+          case "Galería":
+            arma.innerHTML = "trofeo";
+            break;
+          case "Sala de billar":
+            arma.innerHTML = "palo de billar";
+            break;
+          case "Comedor":
+            arma.innerHTML = "cuclillo";
+            break;
+        
+          default:
+            break;
+        }
+
+        resuelto.innerHTML = "Resuelto!!!";
+        // se muestra en consola si coinciden valorSala con cospechoso
+        console.log("el valor coincide")
+    }else {
+
+      //valores iniciales al volver a presionar el boton con coincidencia erronea
+      asesino.innerHTML = "_________"; 
+      sala.innerHTML = "_________"; 
+      arma.innerHTML = "_________";
+      resuelto.innerHTML="";
+
+      // se muestra en consola si no coinciden valorSala con cospechoso
+      console.log("el valor no coincide");
+
     }
+
+    
+
+
 }
